ef5e6ce3f793d5cda26d0e1e310d6a32aee3f833
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2023-10-04 14:51:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(arrayOfRecord) {
+    const winRecord = arrayOfRecord.find(ele => ele.result === 'W')
+    return winRecord ? winRecord.year : undefined;
+}
